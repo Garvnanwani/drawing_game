c72ebe35c05a251f2936b29610ead92d5306b82a
Garvnanwani <garvnanwani88@gmail.com>
2020-12-27 14:18:32 +0530

    set up view engine

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import express from "express"
+import path from "path"
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.set("view engine", "pug")
+
+app.set("views", path.join(__dirname, "views"))
+
+app.get("/", (req, res) => {
+  res.render("home")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`)
