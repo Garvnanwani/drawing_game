820b8c71c6dfd88103e37fa2c1fd260dd0c64df0
Garvnanwani <garvnanwani88@gmail.com>
2020-12-27 16:10:31 +0530

    added socket.io

--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
 import express from "express"
 import path from "path"
+import * as io from "socket.io"
+
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.set("view engine", "pug")
-
 app.set("views", path.join(__dirname, "views"))
+app.use(express.static(path.join(__dirname, "static")))
 
 app.get("/", (req, res) => {
   res.render("home")
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`)
 })
+
+const socketio = new io.Server(server)
