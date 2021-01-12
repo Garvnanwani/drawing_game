065a7105e32fc2b9529e7bfb68d0d60a8c355edf
Garvnanwani <garvnanwani88@gmail.com>
2021-01-12 21:03:10 +0530

    login form data saved

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,21 @@
 const body = document.querySelector("body")
 const nickname = localStorage.getItem("nickname")
+const loginForm = document.getElementById("jsLogin")
+
+const handleFormSubmit = (e) => {
+  e.preventDefault()
+  const input = document.querySelector("input")
+  const { value } = input
+  localStorage.setItem("nickname", value)
+  input.value = ""
+}
 
 if (!nickname) {
   body.classList.add("loggedOut")
 } else {
   body.classList.add("loggedIn")
+}
+
+if (loginForm) {
+  loginForm.addEventListener("submit", handleFormSubmit)
 }
